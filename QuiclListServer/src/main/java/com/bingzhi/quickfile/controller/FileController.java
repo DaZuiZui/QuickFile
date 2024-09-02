97756e2927dcbb700b9b515efa1a495d8eb21abc
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2024-09-02 10:03:03 +0800

    后端项目基站

---
+++ QuiclListServer/src/main/java/com/bingzhi/quickfile/controller/FileController.java
@@ -0,0 +1,14 @@
+package com.bingzhi.quickfile.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Bryan Yang
+ * QuickList controlelr
+ */
+@RestController
+@RequestMapping("/quickList")
+public class FileController {
+
+}
