35f8b1b5a648969391f144113693a337146b9676
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2024-09-02 09:59:16 +0800

    Add files via upload
    分片上传后端

---
+++ QuiclListServer/src/test/java/com/bingzhi/quickfile/QuickFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingzhi.quickfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
